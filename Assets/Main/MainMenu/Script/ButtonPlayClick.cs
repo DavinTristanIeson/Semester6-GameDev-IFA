7e10b378f566c93052b5bc23d82ad971b79b9aaa
Davin Tristan Ieson <davin.tristan@gmail.com>
2024-05-03 22:48:04 +0700

    refine: refine select difficulty screen

--- Assets/Main/MainMenu/Script/ButtonPlayClick.cs
+++ Assets/Main/MainMenu/Script/ButtonPlayClick.cs
@@ -1,12 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonPlay_Click : MonoBehaviour
-{
+public class ButtonPlay_Click : MonoBehaviour {
 
-    public void OpenScene(string sceneName){
-        SceneManager.LoadSceneAsync (sceneName);
-    }
+  public void OpenScene(string sceneName){
+    SceneManager.LoadSceneAsync (sceneName);
+  }
 }
